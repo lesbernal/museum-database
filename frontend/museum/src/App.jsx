4eadf56d566dabcb00c0e8192e167c39a6fd4735
rpham0306 <ryanlampham1@gmail.com>
2026-03-25 01:14:23 -0500

    navbar.jsx and app.jsx modifications

--- frontend/museum/src/App.jsx
+++ frontend/museum/src/App.jsx
@@ -1,36 +1,42 @@
 // App.jsx
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Navbar from "./components/Navbar";  // Add this import
-import "./styles/theme.css"; 
+import Navbar from "./components/Navbar";
+import "./styles/theme.css";
 import "./App.css";
 
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import ArtistsGallery from "./pages/ArtistsGallery";
+import Artworks from "./pages/Artworks";
+import Exhibitions from "./pages/Exhibitions";
+import Events from "./pages/Events";
 import AdminDashboard from "./components/AdminDashboard";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />  {/* Add Navbar here - it will show on all pages */}
+      <Navbar />
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/artists" element={<ArtistsGallery />} />
-        
+        <Route path="/artworks" element={<Artworks />} />
+        <Route path="/exhibitions" element={<Exhibitions />} />
+        <Route path="/events" element={<Events />} />
+
         {/* Admin Only Routes */}
-        <Route 
-          path="/admin" 
+        <Route
+          path="/admin"
           element={
             <ProtectedRoute requiredRole="admin">
               <AdminDashboard />
             </ProtectedRoute>
-          } 
+          }
         />
-        
+
         {/* Fallback route */}
         <Route path="*" element={<h1>404 - Page Not Found</h1>} />
       </Routes>
